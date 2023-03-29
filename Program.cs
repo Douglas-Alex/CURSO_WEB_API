94dac1ce86bff1b6fbefc4766f6850d2c310747b
douglasalex.dab@gmail.com <douglasalex.dab@gmail.com>
2023-03-29 03:35:41 -0300

    Curso .NET WEB 6

---
+++ Program.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+var configuration = app.Configuration;
+ProductRepository.Init(configuration);
+
+app.MapPost("/product", (Product products) =>
+{
+    ProductRepository.Add(products);
+    return Results.Created($"/product/{products.Code}", $"{products.Code} - {products.Name}" );
+});
+
+app.MapGet("/product/{code}", ([FromRoute] string code) => 
+{
+    var product = ProductRepository.GetBy(code);
+    if(product != null)
+        return Results.Ok(product);
+    else
+        return Results.NotFound();
+});
+
+app.MapPut("/product", (Product product) =>
+{
+    var productSaved = ProductRepository.GetBy(product.Code);
+    productSaved.Name = product.Name;
+    return Results.Ok();
+});
+
+app.MapDelete("/product/{code}", ([FromRoute] string code) =>
+{
+    var productSaved = ProductRepository.GetBy(code);
+    ProductRepository.Remove(productSaved);
+    return Results.Ok();
+});
+
+app.MapGet("/configuration/database", (IConfiguration configuration) =>
+{
+    return Results.Ok(configuration["database:conection"]);
+});
+
+app.Run();
